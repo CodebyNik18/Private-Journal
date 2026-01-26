f6247789e401de4a5a2b70cb82f8a6612d8e2dc5
Nikhil <1837nik@gmail.com>
2026-01-26 22:05:42 +0530

    Add Sign in functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 def sign_in(request):
+    if request.method == 'POST':
+        name = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        User.objects.create_user(name, email, password)
+        
+        messages.success(request, "Account created successfully. You can now log in.")
+        
+        return redirect('login')
     return render(request=request, template_name='sign_up.html')
 
 def login(request):
