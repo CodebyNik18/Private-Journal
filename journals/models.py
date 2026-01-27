07bf2643482180bcf536dfdae78cf4743c6d781b
Nikhil <1837nik@gmail.com>
2026-01-27 23:15:40 +0530

    complete add journal functionality

--- journals/models.py
+++ journals/models.py
@@ -9,4 +9,4 @@ class AddJournal(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-        return self.title
+        return f"{self.user.username} - { self.title}"
--- journals/views.py
+++ journals/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import AddJournal
 
 
 def add(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        AddJournal.objects.create(title=title, content=content, user=request.user)
+        return redirect('journals_notes')
     return render(request=request, template_name='add_journal.html')
