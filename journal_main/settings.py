9713bf122ce509b11ef2d4ceac754f2a35659572
Nikhil <1837nik@gmail.com>
2026-01-28 17:27:41 +0530

    Add sqlite fallback for local development

--- journal_main/settings.py
+++ journal_main/settings.py
@@ -83,9 +83,27 @@ WSGI_APPLICATION = 'journal_main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-}
+import os
+
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 
